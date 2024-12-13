85191b6f3700bbae0ba9df55f7f501a6cc8a1a31
Basit ali <basitalyshah51214@gmail.com>
2024-12-13 08:51:29 -0800

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:deal_or_not_deal/pages/FirstPage/first_page.dart';
-import 'package:deal_or_not_deal/pages/SelectUser/input_form.dart';
-import 'package:deal_or_not_deal/pages/splash_page/splash_page.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
