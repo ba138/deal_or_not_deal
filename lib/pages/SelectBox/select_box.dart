9844b5b9a23da336c7fa71e52e6263c697917f32
Basit ali <basitalyshah51214@gmail.com>
2024-12-19 03:29:41 -0800

    show case image in select prefernce cases

--- lib/pages/SelectBox/select_box.dart
+++ lib/pages/SelectBox/select_box.dart
@@ -1,3 +1,4 @@
+import 'package:deal_or_not_deal/utills/res.dart';
 import 'package:flutter/material.dart';
 
 class SelectBox extends StatelessWidget {
@@ -6,6 +7,60 @@ class SelectBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(
+            30,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 7,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 20.0,
+                    childAspectRatio: 1.5,
+                  ),
+                  itemCount: cases.length,
+                  itemBuilder: (context, index) {
+                    String caseImage = cases[index];
+                    return InkWell(
+                      onTap: () {},
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Image.asset(
+                                caseImage,
+                                fit: BoxFit.contain,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
