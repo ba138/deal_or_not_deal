bb2be5576b12b3adc4f7d26b32139ec84f96b8f7
Basit ali <basitalyshah51214@gmail.com>
2024-12-23 08:41:41 -0800

    Refactor SelectBox layout to improve UI and enhance background image display

--- lib/pages/SelectBox/select_box.dart
+++ lib/pages/SelectBox/select_box.dart
@@ -2,7 +2,6 @@ import 'package:deal_or_not_deal/pages/splash_page/splash_page.dart';
 import 'package:deal_or_not_deal/utills/res.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 class SelectBox extends StatelessWidget {
   final String userName;
@@ -12,71 +11,81 @@ class SelectBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 1.12,
-                // Use Expanded to make GridView flexible
-                child: Padding(
-                  padding:
-                      const EdgeInsets.only(left: 10.0, right: 20, bottom: 40),
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            // mainAxisExtent: 200,
-                            // maxCrossAxisExtent:
-                            //     200, // Max width for each grid item
+        child: Container(
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/splash_image.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 1.12,
+                  // Use Expanded to make GridView flexible
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 20, bottom: 40),
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              // mainAxisExtent: 200,
+                              // maxCrossAxisExtent:
+                              //     200, // Max width for each grid item
 
-                            crossAxisCount: 7,
-                            crossAxisSpacing: 20,
-                            mainAxisSpacing: 20.0,
-                            childAspectRatio: 1.38),
-                    itemCount: cases.length,
-                    itemBuilder: (context, index) {
-                      String caseImage = cases[index];
-                      return InkWell(
-                        onTap: () {
-                          selectBox(caseImage);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            elevation: 4,
-                            child: Container(
-                              width: 200,
-                              decoration: BoxDecoration(
-                                color: Colors.transparent,
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 2,
-                                ),
+                              crossAxisCount: 7,
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20.0,
+                              childAspectRatio: 1.38),
+                      itemCount: cases.length,
+                      itemBuilder: (context, index) {
+                        String caseImage = cases[index];
+                        return InkWell(
+                          onTap: () {
+                            selectBox(caseImage);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(8),
                               ),
-                              child: Stack(
-                                alignment: Alignment.center,
-                                children: [
-                                  Image.asset(
-                                    caseImage,
-                                    fit: BoxFit.contain,
-                                    height: 200,
-                                    width: 200,
+                              elevation: 4,
+                              child: Container(
+                                width: 200,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Colors.white,
+                                    width: 2,
                                   ),
-                                ],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Image.asset(
+                                      caseImage,
+                                      fit: BoxFit.contain,
+                                      height: 200,
+                                      width: 200,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
